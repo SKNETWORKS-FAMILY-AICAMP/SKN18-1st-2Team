8caeb4e18d396637917411af16a3aeac967a7050
JungyuOO <rlawnsrb731@gmail.com>
2025-08-01 15:54:09 +0900

    fix: 테이블 구조, 삽입 데이터 수정

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -5,23 +5,7 @@ USE sknproject1;
 -- 전기차 등록현황
 CREATE TABLE IF NOT EXISTS ev_yearly_stats (
     year VARCHAR(10),
-    seoul VARCHAR(50),
-    busan VARCHAR(50),
-    daegu VARCHAR(50),
-    incheon VARCHAR(50),
-    gwangju VARCHAR(50),
-    daejeon VARCHAR(50),
-    ulsan VARCHAR(50),
-    sejong VARCHAR(50),
-    gyeonggi VARCHAR(50),
-    gangwon VARCHAR(50),
-    chungbuk VARCHAR(50),
-    chungnam VARCHAR(50),
-    jeonbuk VARCHAR(50),
-    jeonnam VARCHAR(50),
-    gyeongbuk VARCHAR(50),
-    gyeongnam VARCHAR(50),
-    jeju VARCHAR(50),
+    region VARCHAR(50),
     total VARCHAR(50)
 );
 
