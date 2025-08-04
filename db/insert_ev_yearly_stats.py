1c554f482fb77ab02096caad1b6be01e7afad65d
HJincode <catehj22@gmail.com>
2025-08-04 12:49:57 +0900

    feat: 데이터 처리 함수화

--- db/insert_ev_yearly_stats.py
+++ db/insert_ev_yearly_stats.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import pymysql
 
-conn = pymysql.connect(
-    host='localhost',
-    user='root',           
-    password='root1234',     
-    db='sknproject1',       
-    charset='utf8mb4'
-)
+def insert_yearly_ev_data():
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',           
+        password='root1234',     
+        db='sknproject1',       
+        charset='utf8mb4'
+    )
 
-cursor = conn.cursor()
-cursor.execute("TRUNCATE TABLE ev_yearly_stats")
-conn.commit()
+    cursor = conn.cursor()
+    cursor.execute("TRUNCATE TABLE ev_yearly_stats")
+    conn.commit()
 
-df = pd.read_csv('../data/ev_yearly_stats_long.csv')   
-for _, row in df.iterrows():
-    cursor.execute("""
-        INSERT INTO ev_yearly_stats (year, region, total)
-        VALUES (%s, %s, %s)
-    """, tuple(row))
-conn.commit()
-print("ev_yearly_stats 입력 완료")
-conn.close()
+    df = pd.read_csv('../data/ev_yearly_stats_long.csv')   
+    for _, row in df.iterrows():
+        cursor.execute("""
+            INSERT INTO ev_yearly_stats (year, region, total)
+            VALUES (%s, %s, %s)
+        """, tuple(row))
+    conn.commit()
+    print("ev_yearly_stats 입력 완료")
+    conn.close()
