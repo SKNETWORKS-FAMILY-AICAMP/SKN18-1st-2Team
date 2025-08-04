2e3630761ba8332e1b4981bffdd5f54357ff6308
JungyuOO <rlawnsrb731@google.com>
2025-08-04 14:43:39 +0900

    feat: DB에 데이터 삽입 절대 경로 지정

--- db/insert_ev_yearly_stats.py
+++ db/insert_ev_yearly_stats.py
@@ -1,12 +1,13 @@
+import os
 import pandas as pd
 import pymysql
 
 def insert_yearly_ev_data():
     conn = pymysql.connect(
         host='localhost',
-        user='root',           
-        password='root1234',     
-        db='sknproject1',       
+        user='root',
+        password='root1234',
+        db='sknproject1',
         charset='utf8mb4'
     )
 
@@ -14,7 +15,13 @@ def insert_yearly_ev_data():
     cursor.execute("TRUNCATE TABLE ev_yearly_stats")
     conn.commit()
 
-    df = pd.read_csv('../data/ev_yearly_stats_long.csv')   
+    # 현재 파일 기준으로 프로젝트 루트의 data 폴더 내 csv 절대경로 지정
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    csv_path = os.path.join(project_root, "data", "ev_yearly_stats_long.csv")
+    csv_path = os.path.normpath(csv_path)
+    print("읽는 경로:", csv_path)
+
+    df = pd.read_csv(csv_path)
     for _, row in df.iterrows():
         cursor.execute("""
             INSERT INTO ev_yearly_stats (year, region, total)
