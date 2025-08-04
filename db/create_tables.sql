bd3b0acee9154b682fbd06dd2025f8984060a927
JungyuOO <rlawnsrb731@google.com>
2025-08-04 12:31:04 +0900

    feat: sql table 수정, insert db python 수정, data csv 추가

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -4,16 +4,18 @@ USE sknproject1;
 
 -- 전기차 등록현황
 CREATE TABLE IF NOT EXISTS ev_yearly_stats (
-    year VARCHAR(10),
-    region VARCHAR(50),
-    total VARCHAR(50)
+ 	year CHAR(4),
+	region VARCHAR(20),
+    total INT NOT NULL,
+    PRIMARY KEY(year, region)
 );
 
 -- FAQ 테이블
 CREATE TABLE IF NOT EXISTS kia_faq_data (
+	faq_id INT AUTO_INCREMENT PRIMARY KEY, 
     category VARCHAR(100),
-    question TEXT,
-    answer_text TEXT,
+    question TEXT NOT NULL,
+    answer_text TEXT NOT NULL,
     image_urls TEXT,
     link_urls TEXT
 );
